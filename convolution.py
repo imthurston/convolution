a54bacd8266de2832f314073695f5e1cd12a748b
Ian Thurston <imthurston@wisc.edu>
2023-12-14 06:08:09 -0600

    Fixed issues from using wrong repo

--- convolution.py
+++ convolution.py
@@ -12,6 +12,18 @@ from matplotlib.animation import FuncAnimation
 #--------------------------------------------------------------------------------------------------------------------
 #Class Definitions---------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------
+class UniverseConvolution:
+    def __init__(self, conf_path, traj_path):
+        self.universe = mda.Universe(conf_path,traj_path)
+    
+    def create_shifted_conc_df(self):
+        pass
+    
+
+
+
+
+
 class FrameConvolution:
     #----------------------------------------------------------------------------------------------------------------
     #Special methods-------------------------------------------------------------------------------------------------
@@ -24,6 +36,7 @@ class FrameConvolution:
             self.df = pd.read_csv(csv_path, index_col=False)
         except FileNotFoundError:
             self.df = self.gen_frame_df()
+        self.molecules = set(self.df.ResType)
         self.x_dim = self.universe.trajectory[0].dimensions[0]
         self.y_dim = self.universe.trajectory[0].dimensions[1]
         self.z_dim = self.universe.trajectory[0].dimensions[2]
@@ -33,7 +46,7 @@ class FrameConvolution:
     #Frame DF Generation---------------------------------------------------------------------------------------------
     #----------------------------------------------------------------------------------------------------------------     
     def gen_frame_df(self):
-        atom_radius_mapping = {'C': .914, 'H': .5, 'N': .92, "O": .73}
+        atom_radius_mapping = {'C': .914, 'H': .5, 'N': .92, "O": .73, "A":1 , "B":1 }
         data_dict = {'AtomID': [], "AtomName":[], 'AtomType': [], "ResType": [], "ResID": [], 'X': [], 'Y': [], 'Z': []}
         for atom in self.universe.atoms:
             #print(atom)
@@ -79,18 +92,29 @@ class FrameConvolution:
     #Analysis Methods------------------------------------------------------------------------------------------------
     #----------------------------------------------------------------------------------------------------------------
     def create_conc_df(self, travel_axis = "Z", bins = 1000):
-        data_dict = {travel_axis: [], "NicConc":[], 'SolConc': [] }
+        #Initialize the data dictionary to store the concentration with the residue concentrations
+        data_dict = {travel_axis: []}
+        for molecule in self.molecules:
+            data_dict[f"{molecule}Conc"] = []
+        #data_dict = {travel_axis: [], "NicConc":[], 'SolConc': [] }
+        
+        
         travel_axis_dim = self.get_dimension(travel_axis) 
         travel_axis_thickness = travel_axis_dim / bins #i.e, delta Z 
         for i in range(bins):
             travel_axis_val = i * travel_axis_thickness
             #filtered_df = filter_df(d_travel,z)
+            
+            
+            #vol_d in format: vol_d = {"Total":total_vol, "Nicotine":nic_vol,"Solvent":sol_vol}
             vol_d = self.calc_conc(travel_axis, travel_axis_val, travel_axis_thickness)
-            nic_conc = vol_d["Nicotine"]/vol_d["Total"]
-            sol_conc = vol_d["Solvent"]/vol_d["Total"]
+            
             data_dict[travel_axis].append(travel_axis_val)
-            data_dict["NicConc"].append(nic_conc)
-            data_dict["SolConc"].append(sol_conc)
+            for molecule in self.molecules:
+                mol_conc = vol_d[molecule]/vol_d["Total"]
+                #sol_conc = vol_d["Solvent"]/vol_d["Total"]
+                data_dict[f"{molecule}Conc"].append(mol_conc)
+                
         conc_df = pd.DataFrame(data_dict)
         return conc_df    
     
@@ -98,7 +122,7 @@ class FrameConvolution:
         df = self.create_autocorr_df(travel_axis, molecule_conc, bins)
         df.plot(kind="scatter", x="d",y="c_aa")
         return None
-        
+    #The autocorrelator function is the convlolutional method we're applying   
     def create_autocorr_df(self,travel_axis = "Z",molecule_conc = "NicConc", bins = 1000):
         caa_d = {"d":[],"c_aa": []}
         travel_axis_dim = self.get_dimension(travel_axis)
@@ -112,20 +136,89 @@ class FrameConvolution:
             caa_d["c_aa"].append(c_aa)
         return pd.DataFrame(caa_d)
         
-    def plot_conc_scatter(self, travel_axis, molecule_conc="NicConc",bins=1000):
+    def plot_conc_scatter(self, travel_axis="Z", molecule_conc="NicConc",bins=1000):
         df = self.create_conc_df(travel_axis, bins)
         df.plot(kind="scatter",x=travel_axis, y=molecule_conc)
-                
+    
+    def create_pulse_df(self,travel_axis="Z", molecule_conc="NicConc", bins=1000):
+        df = self.create_autocorr_df(travel_axis, molecule_conc,bins)
+        d = {"d":[],"pulse":[]}
+        for row in df.T:
+            d["d"].append(df.iloc[row].d)
+            if df.iloc[row].c_aa > 0:
+                d["pulse"].append(1)
+            else:
+                d["pulse"].append(0)
+        return pd.DataFrame(d)
+    
+    def plot_pulse_scatter(self,travel_axis = "Z", molecule_conc="NicConc", bins=1000):
+        df = self.create_pulse_df(travel_axis, molecule_conc, bins)
+        df.plot(kind="scatter", x="d",y="pulse")
+        return None
+    
+    def create_Xc_df(self,travel_axis = "Z",molecule_conc = "NicConc", bins = 1000):
+        pulse_df = self.create_pulse_df(travel_axis, molecule_conc)
+        conc_df = self.create_conc_df(travel_axis=travel_axis, bins=len(pulse_df))
+        pulse_tensor = torch.tensor(np.array(pulse_df.pulse), dtype=torch.double)
+        conc_tensor = torch.tensor(np.array(conc_df[molecule_conc]))
+        
+        Xc_d = {"n":[],"Xc": []}
+        travel_axis_dim = self.get_dimension(travel_axis)
+        travel_midpoint = travel_axis_dim/2
+        for n in range(len(pulse_df)):
+            #print(d)
+            X_c =self.X_c(n, conc_tensor, pulse_tensor)
+            Xc_d["n"].append(n)
+            Xc_d["Xc"].append(X_c)
+        return pd.DataFrame(Xc_d)
+    
+    def plot_Xc_scatter(self, travel_axis="Z", molecule_conc="NicConc",bins=1000):
+        df = self.create_Xc_df(travel_axis, molecule_conc, bins)
+        df.plot(kind="scatter", x="n",y="Xc")
+        return None
+    
+    """    
+    def create_shifted_conc_df(self,travel_axis = "Z",molecule_conc = "NicConc", bins = 1000):
+        conc_df = self.create_conc_df(travel_axis, bins)
+        Xc_df = self.create_Xc_df(travel_axis,molecule_conc, bins)
+        n_max = int(Xc_df[Xc_df.Xc == Xc_df.Xc.max()].n)
+        print(f"DEBUG n_max:{n_max}")
+        conc_df["Z"] = conc_df["Z"].apply(lambda x: x + n_max)
+        return conc_df
+    """
+    
+    def create_shifted_conc_df(self, travel_axis="Z", molecule_conc="NicConc", bins=1000):
+        conc_df = self.create_conc_df(travel_axis, bins)
+        Xc_df = self.create_Xc_df(travel_axis, molecule_conc, bins)
+        n_max = int(Xc_df[Xc_df.Xc == Xc_df.Xc.max()].n)
+        print(f"DEBUG n_max: {n_max}")
+        n_max = -n_max
+        max_z = conc_df[travel_axis].max()
+        conc_df["Z"] = conc_df["Z"].apply(lambda x: (x + n_max) % (max_z + 1))
+
+        return conc_df    
+    
+    
+    
+    
+    def plot_shifted_conc_scatter(self, travel_axis="Z", molecule_conc="NicConc",bins=1000):
+        df = self.create_shifted_conc_df(travel_axis, molecule_conc, bins)
+        df.plot(kind="scatter", x=travel_axis,y=molecule_conc)
+        return None
+        
     #Analysis Helper Methods-----------------------------------------------------------------------------------------
     #----------------------------------------------------------------------------------------------------------------
     def calc_conc(self, travel_axis, travel_axis_val, travel_axis_thickness):
         # Initialize variables for total area and areas of each residue
-        area_unk = 0
-        area_sol = 0
+        area_d = {}
         #This makes a df that has all atoms that are too far away to project into the slice filtered out. 
         #This is NOT self.df!!!
         #Don't get confused.
         df = self.filter_df(travel_axis, travel_axis_val)
+        
+        for residue in set(self.df.ResType):
+            area_d[residue]=0
+        
         # Iterate through each atom in the DataFrame
         for index, atom in df.iterrows():
             # Calculate the radius for the slice at Z(or rather, the travel axis
@@ -144,10 +237,12 @@ class FrameConvolution:
             if radius > 0:
                 area = np.pi * radius**2
                 # Add the area to the corresponding residue
-                if atom['ResType'] == 'UNK':
-                    area_unk += area
-                elif atom['ResType'] == 'SOL':
-                    area_sol += area
+                area_d[atom['ResType']] += area
+                
+               #if atom['ResType'] == 'UNK':
+                #    area_unk += area
+                #elif atom['ResType'] == 'SOL':
+                #    area_sol += area
         
         #Objects aren't subscriptable unfortunately so I need these stupid if then clauses to extract the
         #orthoganol axis dimensions.
@@ -166,10 +261,13 @@ class FrameConvolution:
 
         #This aspect of the code won't translate to other systems
         #TODO modify to implement a general case
-        nic_vol = area_unk * travel_axis_thickness
-        sol_vol = area_sol * travel_axis_thickness
+        vol_d = {"Total":total_vol}
+        for res in area_d:
+            vol_d[res] = area_d[res] * travel_axis_thickness
+        #nic_vol = area_unk * travel_axis_thickness
+        #sol_vol = area_sol * travel_axis_thickness
         # Print the calculated areas
-        vol_d = {"Total":total_vol, "Nicotine":nic_vol,"Solvent":sol_vol}
+        #vol_d = {"Total":total_vol, "Nicotine":nic_vol,"Solvent":sol_vol}
         #print(f"Total Area: {total_area}")
         #print(f"Area of UNK: {area_unk}")
         #print(f"Area of SOL: {area_sol}")
@@ -194,10 +292,15 @@ class FrameConvolution:
         c_aa = torch.dot(delta_conc_z, delta_conc_zminusd)
         return float(c_aa)
         
+    def X_c(self,n, conc_tensor, pulse_tensor):
+        shifted_tensor = pulse_tensor.roll(n)
+        dot_prod = torch.dot(conc_tensor, shifted_tensor)
+        return float(dot_prod)
+        
     #----------------------------------------------------------------------------------------------------------------    
     #Visualization Methods-------------------------------------------------------------------------------------------
     #----------------------------------------------------------------------------------------------------------------   
-    
+
     #TODO Finish this such that I can actually make an animation
     def create_animation(self, travel_axis, bins):
         # Set up the animation
@@ -223,10 +326,12 @@ class FrameConvolution:
     def plot_slice(self, travel_axis, travel_axis_val):
             fig, ax = plt.subplots()
             # Initialize variables for total area and areas of each residue
-            total_area = 0
-            area_unk = 0
-            area_sol = 0
-
+            area_d = {}
+            df = self.filter_df(travel_axis, travel_axis_val)
+        
+            for residue in set(self.df.ResType):
+                area_d[residue]=0
+            
             #define the orthoganal axes based off the travel axis
             if travel_axis == "X":
                 orth_dim_1 = self.y_dim
@@ -243,8 +348,6 @@ class FrameConvolution:
                 orth_axis_1 = "X"
                 orth_dim_2 = self.y_dim
                 orth_axis_2 = "Y"
-            
-            df = self.filter_df(travel_axis, travel_axis_val)
             # Iterate through each atom in the DataFrame
             for index, atom in df.iterrows():
                 # Calculate the radius for the slice at Z
@@ -253,7 +356,6 @@ class FrameConvolution:
                     #print("invalid projection")
                     continue
                 radius = np.sqrt(atom['Radius']**2 - (atom[travel_axis] - travel_axis_val)**2)
-                
                 # Map different face colors based on Res
                 #face_color = 'r' if atom['ResType'] == 'UNK' else 'b'
                 if atom.AtomType == "C":
@@ -264,20 +366,14 @@ class FrameConvolution:
                     face_color = "r"
                 if atom.AtomType == "N":
                     face_color = "b"
-                
+                if atom.AtomType == "A":
+                    face_color = "k"
+                if atom.AtomType == "B":
+                    face_color = "w"
                 # Plot the circle with different face color for each ResID
                 circle = plt.Circle((atom[orth_axis_1], atom[orth_axis_2]), radius, edgecolor='k', facecolor=face_color, alpha=0.5)
                 ax.add_patch(circle)
-            
-                # Calculate the area of the circle using A = π * r^2
-                if radius > 0:
-                    area = np.pi * radius**2
-                    # Add the area to the corresponding residue
-                    if atom['ResType'] == 'UNK':
-                        area_unk += area
-                    elif atom['ResType'] == 'SOL':
-                        area_sol += area
-        
+                
             # Set plot limits
             ax.set_xlim(0,orth_dim_1)
             ax.set_ylim(0,orth_dim_2)
